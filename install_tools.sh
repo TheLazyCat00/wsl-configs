758b95a51a06c5f91d3a889cbf527cd890297e6a
TheLazyCat00 <manuel.stieger@proton.me>
2026-02-03 22:28:33 +0100

    update

--- install_tools.sh
+++ install_tools.sh
@@ -11,27 +11,3 @@ sudo apt install python3.12-venv
 echo "Installing brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-echo "Installing tools"
-brew install neovim
-brew install fzf
-brew install composer
-brew install fd
-brew install gh
-brew install lazygit
-brew install ripgrep
-brew install unzip
-brew install sqlite3
-brew install oh-my-posh
-
-echo "Installing languages"
-brew install cmake
-brew install go
-brew install julia
-brew install lua
-brew install luarocks
-brew install node
-brew install openjdk
-brew install python3
-brew install ruby
-brew install rust
